712285c11d3a2b8df45c08c1111344aa843656fb
clement glt <guillemot.clement@protonmail.com>
2024-04-12 22:51:32 +0200

    formulaire nouvel article

--- add-article.php
+++ add-article.php
@@ -1,56 +1,97 @@
-<pre>
+
 <?php 
+// DECLARATION DU FILENAME POUR STOCKER LES DONNEES
+$filename = __DIR__ . '/data/articles.json';
 
 // DECLARATION CONSTANTE MSG D'ERREUR
 const ERROR_REQUIRED = "Saisir une valeur";
-const ERROR_MIN_LENGTH = "La saisie est inférieur à 3 caractères";
-const ERROR_MAX_LENGTH = "La saisie est supérieur à 50 caractères";
-$errors = [];
+const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
+const ERROR_CONTENT_TOO_SHORT = 'Article trop court';
+const ERROR_PIC_URL = 'Image doit etre une url valide';
 
+// INITIALISATION TABLEAU ERREURS
+$errors = [
+    'title' => '',
+    'picture' => '',
+    'category' => '',
+    'content' => ''
+];
+
+// DECLARATIOM D'UIN TABLEAU VIDE POUR LES ARTICLES
+$articles = [];
+
+//VERIFICATION METHODE ENVOI
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    // on verifie que le fichier existe
+    if(file_exists($filename)){
+        // si il existe on le decode et on recupere les donnes
+        $articles = json_decode(file_get_contents($filename), true) ?? [];
+    }
     // filtration des données saisis par l'user
     $_POST = filter_input_array(
         INPUT_POST,
         [
             'title' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-            'picture' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'picture' => FILTER_SANITIZE_URL,
             'category' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-            'content' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-        ]
-    );
+            'content' => [
+                'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+                'flag' => FILTER_FLAG_NO_ENCODE_QUOTES
+            ],
+        ]);
     //recupération des données
     $title = $_POST['title'] ?? '';
     $picture = $_POST['picture'] ?? '';
     $category = $_POST['category'] ?? '';
     $content = $_POST['content'] ?? '';
-};
 
-//validation du formulaire
-if(!$title){
-    $errors['title'] = ERROR_REQUIRED;
-}elseif(mb_strlen($title) < 3){
-    $errors['title'] = ERROR_MIN_LENGTH;
-}elseif(mb_strlen($title) > 50){
-    $errors = ERROR_MAX_LENGTH;
+    // VALIDATION DES DONNES DU FORMULAIRE
+    if(!$title){
+        $errors['title'] = ERROR_REQUIRED;
+    }elseif(mb_strlen($title) < 5){
+        $errors['title'] = ERROR_TITLE_TOO_SHORT;
+    }
+
+    if(!$picture){
+        $errors['picture'] = ERROR_REQUIRED;
+    }elseif(!filter_var($picture, FILTER_VALIDATE_URL)){
+        $errors['picture'] = ERROR_PIC_URL;
+    }
+
+    if(!$category){
+        $errors['category'] = ERROR_REQUIRED;
+    }
+
+    if(!$content){
+        $errors['content'] = ERROR_REQUIRED;
+    }elseif(mb_strlen($content) < 10){
+        $errors['content'] = ERROR_CONTENT_TOO_SHORT;
+    }
+
+    // VERIF $ERRORS VIDE
+    // on utiliser la methode pour verifier que le tableau est vide
+    // $e represente l'iteration en cours
+    // cela permet de retourner une erreur si une valeur est dans le tableau d'erreurs
+    // la methode retourne un nouveau tableau si une erreur est ajouter dans le tableau d'erreur
+
+    // empty verifie que le tableau est vide
+    //si il est vide, alors on rentre dans le if
+    if(empty(array_filter($errors, fn($e) => $e !== ''))){
+        //on recup les donnes du nouvel article et on ajoute dans le tableau d'article
+        $articles = [...$articles, [
+            'id' => time(),
+            'title' => $title,
+            'picture' => $picture,
+            'category' => $category,
+            'content' => $content
+        ]];
+        //encode et on ecrase ancien fichier
+        file_put_contents($filename, json_encode($articles));
+        //on renvoie user vers l'accueil
+        header('Location: /');
+    }
 }
-
-
-
-
 ?>
-</pre>
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -68,14 +109,17 @@ if(!$title){
                 <form action="/add-article.php" method="post">
                     <div class="form-control">
                         <label for="title">Titre</label>
-                        <input type="text" name="title" id="title" value="<?= $title ?? '' ?>>
-                        <?= $errors['title'] ? '<p style="color: red">'.$errors['title'].'</p>' : "" ?>
-                        
+                        <input type="text" name="title" id="title" value="<?= $title ?? '' ?>">
+                        <?php if($errors['title']) : ?>
+                            <p class="text-error"><?= $errors['title'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-control">
                         <label for="picture">Image</label>
-                        <input type="text" name="picture" id="picture">
-                        <!-- <p class="text-error"></p> -->
+                        <input type="text" name="picture" id="picture" value="<?= $picture ?? '' ?>">
+                        <?php if($errors['picture']) : ?>
+                            <p class="text-error"><?= $errors['picture'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-control">
                         <label for="category">Catégorie</label>
@@ -83,13 +127,18 @@ if(!$title){
                             <option value="technology">Technologie</option>
                             <option value="nature">Nature</option>
                             <option value="politic">Politique</option>
+                            <option value="sport">Sport</option>
                         </select>
-                        <!-- <p class="text-error"></p> -->
+                        <?php if($errors['category']) : ?>
+                            <p class="text-error"><?= $errors['category'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-control">
                         <label for="content">Contenu</label>
-                        <textarea name="content" id="content"></textarea>
-                        <!-- <p class="text-error"></p> -->
+                        <textarea name="content" id="content" value="<?= $content ?? '' ?>"></textarea>
+                        <?php if($errors['content']) : ?>
+                            <p class="text-error"><?= $errors['content'] ?></p>
+                        <?php endif; ?>
                     </div>
                     <div class="form-action">
                         <button type="submit" class="btn btn-primary" type="button">Sauvegarder</button>
