35557c12961e9f0f16d4746f7bfed1354e858235
Clement Glt <guillemot.clement@protonmail.com>
2024-04-12 17:59:48 +0200

    Travail sur le formulaire partie php

--- add-article.php
+++ add-article.php
@@ -1,3 +1,57 @@
+<pre>
+<?php 
+
+// DECLARATION CONSTANTE MSG D'ERREUR
+const ERROR_REQUIRED = "Saisir une valeur";
+const ERROR_MIN_LENGTH = "La saisie est inférieur à 3 caractères";
+const ERROR_MAX_LENGTH = "La saisie est supérieur à 50 caractères";
+$errors = [];
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    // filtration des données saisis par l'user
+    $_POST = filter_input_array(
+        INPUT_POST,
+        [
+            'title' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'picture' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'category' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'content' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        ]
+    );
+    //recupération des données
+    $title = $_POST['title'] ?? '';
+    $picture = $_POST['picture'] ?? '';
+    $category = $_POST['category'] ?? '';
+    $content = $_POST['content'] ?? '';
+};
+
+//validation du formulaire
+if(!$title){
+    $errors['title'] = ERROR_REQUIRED;
+}elseif(mb_strlen($title) < 3){
+    $errors['title'] = ERROR_MIN_LENGTH;
+}elseif(mb_strlen($title) > 50){
+    $errors = ERROR_MAX_LENGTH;
+}
+
+
+
+
+?>
+</pre>
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +68,9 @@
                 <form action="/add-article.php" method="post">
                     <div class="form-control">
                         <label for="title">Titre</label>
-                        <input type="text" name="title" id="title">
-                        <!-- <p class="text-error"></p> -->
+                        <input type="text" name="title" id="title" value="<?= $title ?? '' ?>>
+                        <?= $errors['title'] ? '<p style="color: red">'.$errors['title'].'</p>' : "" ?>
+                        
                     </div>
                     <div class="form-control">
                         <label for="picture">Image</label>
@@ -37,7 +92,7 @@
                         <!-- <p class="text-error"></p> -->
                     </div>
                     <div class="form-action">
-                        <button class="btn btn-primary" type="button">Sauvegarder</button>
+                        <button type="submit" class="btn btn-primary" type="button">Sauvegarder</button>
                         <a class="btn btn-secondary" type="button" href="/">Annuler</a>
                     </div>
                 </form>
