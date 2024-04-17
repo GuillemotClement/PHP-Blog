b72f145c22ac5c956c6bfae65eaa0d824efb3aca
Clement Glt <guillemot.clement@protonmail.com>
2024-04-17 15:01:51 +0200

    Ajout connexion userI

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,31 @@
+<?php
+	$currentUser = $currentUser ?? false;
+?>
+
 <header>
 	<a href="/" class="icone">
 		<i class="fa-solid fa-cat mr-5"></i>
 		Blogzawesome
 	</a>
 	<ul class="header-menu">
-		<li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? "active" : '' ?>>
-			<a href="/auth-register.php">Inscription</a>
-		</li>
-		<li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? "active" : '' ?>>
-			<a href="/auth-login.php">Connexion</a>
-		</li>
-		<li class=<?= $_SERVER['REQUEST_URI'] === '/auth-logout.php' ? "active" : '' ?>>
-			<a href="/auth-logout.php">Déconnexion</a>
-		</li>
-		<li class=<?= $_SERVER['REQUEST_URI'] === '/profil.php' ? "active" : '' ?>>
-			<a href="/profil.php">Ma page</a>
-		</li>
-		<li class=<?= $_SERVER['REQUEST_URI'] === '/edit-article.php' ? "active" : '' ?>>
-			<a href="/edit-article.php" >Ecrire un article</a>
-		</li>	
+		<?php if($currentUser) : ?>
+			<li class=<?= $_SERVER['REQUEST_URI'] === '/edit-article.php' ? "active" : '' ?>>
+				<a href="/edit-article.php" >Ecrire un article</a>
+			</li>
+			<li class=<?= $_SERVER['REQUEST_URI'] === '/profil.php' ? "active" : '' ?>>
+				<a href="/profil.php">Ma page</a>
+			</li>
+			<li class=<?= $_SERVER['REQUEST_URI'] === '/auth-logout.php' ? "active" : '' ?>>
+				<a href="/auth-logout.php">Déconnexion</a>
+			</li>
+		<?php else :?>
+			<li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? "active" : '' ?>>
+				<a href="/auth-login.php">Connexion</a>
+			</li>
+			<li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? "active" : '' ?>>
+				<a href="/auth-register.php">Inscription</a>
+			</li>
+		<?php endif; ?>
 	</ul>
 </header>
 
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+require __DIR__ . '/database/database.php';
+require __DIR__ . "/database/security.php";
+$currentUser = isLoggedIn();
+
 $articleDB = require_once './database/models/ArticleDB.php';
 $articles = $articleDB->fetchAll();
 $category = [];
@@ -7,76 +11,76 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $selectedCat = $_GET['cat'] ?? '';
 
 if(count($articles)){
-    $cattmp = array_map(fn($a) => $a['category'], $articles);
-    $category = array_reduce($cattmp, function($acc, $cat){
-        if(isset($acc[$cat])){
-            $acc[$cat]++;
-        }else{
-            $acc[$cat] = 1;
-        }
-        return $acc;
-    },[]);
-    $articlePerCategory = array_reduce($articles, function($acc, $article){
-        if(isset($acc[$article['category']])){
-            $acc[$article['category']] = [...$acc[$article['category']], $article];
-        }else{
-            $acc[$article['category']] = [$article];
-        }
-        return $acc;
-    }, []);
+	$cattmp = array_map(fn($a) => $a['category'], $articles);
+	$category = array_reduce($cattmp, function($acc, $cat){
+		if(isset($acc[$cat])){
+			$acc[$cat]++;
+		}else{
+			$acc[$cat] = 1;
+		}
+		return $acc;
+	},[]);
+	$articlePerCategory = array_reduce($articles, function($acc, $article){
+		if(isset($acc[$article['category']])){
+			$acc[$article['category']] = [...$acc[$article['category']], $article];
+		}else{
+			$acc[$article['category']] = [$article];
+		}
+		return $acc;
+	}, []);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require_once './includes/head.php'; ?>
-    <title>Blogzawesome | Accueil</title>
+	<?php require_once './includes/head.php'; ?>
+	<title>Blogzawesome | Accueil</title>
 </head>
 <body>
-    <div class="container">
-        <?php require_once './includes/header.php'; ?>
-        <div class="content">
-            <div class="newsfeed-container">
-                <ul class="category">
-                    <li class=<?= $selectedCat ? "" : "cat-active"?>><a href="/">Tous les articles <span class="small">(<?= count($articles) ?>)</span></a></li>
-                    <?php foreach($category as $catName => $catNum) :?>
-                        <li class=<?= $selectedCat === $catName ? "cat-active" : ""?>><a href="/?cat=<?= $catName ?>"><?= $catName ?><span class="small"><?= $catNum ?></span></a></li>
-                    <?php endforeach;?>
-                </ul>
-                <div class="article">
-                    <?php  if(!$selectedCat):?>
-                    <?php foreach($category as $cat => $num): ?>
-                        <h2><?= $cat ?></h2>
-                        <div class="articles-container">
-                            <?php foreach($articlePerCategory[$cat] as $a): ?>
-                                <a class="article block" href="/show-article.php?id=<?= $a['id']?>">
-                                    <div class="overflow">
-                                        <div class="article-picture" style="background-image: url(<?= $a['picture'] ?>"></div>
-                                    </div>
-                                    <h3><?= $a['title']?></h3>
-                                </a>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endforeach; ?>
-                    <?php else : ?>
-                        <h2><?= $selectedCat ?></h2>
-                        <div class="articles-container">
-                            <?php foreach($articlePerCategory[$selectedCat] as $a): ?>
-                                <a class="article block" href="/show-article.php?id=<?= $a['id']?>">
-                                    <div class="overflow">
-                                        <div class="article-picture" style="background-image: url(<?= $a['picture'] ?>"></div>
-                                    </div>
-                                    <h3><?= $a['title']?></h3>
-                                </a>
-                            <?php endforeach; ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div> 
-        </div>     
-    </div>
-        <?php require_once './includes/footer.php'; ?>
-    </div>
+	<div class="container">
+		<?php require_once './includes/header.php'; ?>
+		<div class="content">
+			<div class="newsfeed-container">
+				<ul class="category">
+					<li class=<?= $selectedCat ? "" : "cat-active"?>><a href="/">Tous les articles <span class="small">(<?= count($articles) ?>)</span></a></li>
+					<?php foreach($category as $catName => $catNum) :?>
+						<li class=<?= $selectedCat === $catName ? "cat-active" : ""?>><a href="/?cat=<?= $catName ?>"><?= $catName ?><span class="small"><?= $catNum ?></span></a></li>
+					<?php endforeach;?>
+				</ul>
+				<div class="article">
+					<?php  if(!$selectedCat):?>
+					<?php foreach($category as $cat => $num): ?>
+						<h2><?= $cat ?></h2>
+						<div class="articles-container">
+							<?php foreach($articlePerCategory[$cat] as $a): ?>
+								<a class="article block" href="/show-article.php?id=<?= $a['id']?>">
+									<div class="overflow">
+										<div class="article-picture" style="background-image: url(<?= $a['picture'] ?>"></div>
+									</div>
+									<h3><?= $a['title']?></h3>
+								</a>
+							<?php endforeach; ?>
+						</div>
+					<?php endforeach; ?>
+					<?php else : ?>
+						<h2><?= $selectedCat ?></h2>
+						<div class="articles-container">
+							<?php foreach($articlePerCategory[$selectedCat] as $a): ?>
+								<a class="article block" href="/show-article.php?id=<?= $a['id']?>">
+									<div class="overflow">
+										<div class="article-picture" style="background-image: url(<?= $a['picture'] ?>"></div>
+									</div>
+									<h3><?= $a['title']?></h3>
+								</a>
+							<?php endforeach; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div> 
+		</div>     
+	</div>
+		<?php require_once './includes/footer.php'; ?>
+	</div>
 </body>
 </html>
